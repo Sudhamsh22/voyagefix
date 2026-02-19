654a0b3ab87bf898fbbbd8907faeb8b03405a1cc
Siva Sudhamsh <sivasudhamsh2005@gmail.com>
2026-02-19 17:02:23 +0000

    https://www.rd.com/wp-content/uploads/2025/05/This-Stunningly-Beautiful-

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,30 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.rd.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.timeout.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
